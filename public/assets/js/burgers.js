58e06eb6f05b803be9600f304849b7d8c19fa04f
Daniel Kubiak <dan.kubiak@gmail.com>
2018-06-21 22:20:38 -0700

    jaw_db

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,4 +1,4 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
 $(function() {
   $(".change-devoured").on("click", function(event) {
     var id = $(this).data("id");
@@ -8,21 +8,21 @@ $(function() {
       devoured: newDevoured
     };
 
-    // Send the PUT request.
+   
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newDevouredState
     }).then(
       function() {
         console.log("changed devoured to", newDevoured);
-        // Reload the page to get the updated list
+        
         location.reload();
       }
     );
   });
 
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
+    
     event.preventDefault();
 
     var newBurger = {
@@ -30,14 +30,14 @@ $(function() {
       devoured: $("[name=devoured]:checked").val().trim()
     };
 
-    // Send the POST request.
+   
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
+        
         location.reload();
       }
     );
@@ -49,11 +49,11 @@ $(function() {
 
     $.ajax("/api/burgers/" + id, {
       type: "DELETE",
-      //data: id
+     
     }).then(
       function() {
         console.log("deleted burger");
-        // Reload the page to get the updated list
+       
         location.reload();
       }
     );
